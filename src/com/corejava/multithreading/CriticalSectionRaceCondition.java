9786a6e8cc2ccbfb0a771d1f7046f7d6cab5c86c
Srikar Vedantam <srikar.vedantam@hpe.com>
2018-09-05 09:02:04 +0530

    Critical Section and Race Condition Demo

---
+++ src/com/corejava/multithreading/CriticalSectionRaceCondition.java
@@ -0,0 +1,77 @@
+package com.corejava.multithreading;
+
+public class CriticalSectionRaceCondition {
+
+	public static void main(String[] args) {
+		// Creating the shared resource
+		BankAccount bankAccount = new BankAccount();
+
+		Depositor depostiorA = new Depositor(100, bankAccount);
+		Depositor depostiorB = new Depositor(200, bankAccount);
+
+		Thread threadA = new Thread(depostiorA, "Thread A");
+		Thread threadB = new Thread(depostiorB, "Thread B");
+
+		System.out.println("Initial bank balance: Rs." + bankAccount.getBalance());
+
+		// Start the threads...
+		threadA.start();
+		threadB.start();
+
+		// Wait for the threads to finish processing
+		try {
+			threadA.join();
+			threadB.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Bank balance after depositing: Rs." + bankAccount.getBalance());
+	}
+
+}
+
+// BankAccount is the shared resource
+class BankAccount {
+	private int balance = 0;
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public void deposit(int amount) {
+		try {
+			Thread.sleep(100); // Doing some processing...
+			int initialBalance = getBalance();
+			Thread.sleep(100); // Doing some processing...
+			int finalBalance = initialBalance + amount;
+			Thread.sleep(100); // Doing some processing...
+			setBalance(finalBalance);
+			Thread.sleep(100); // Doing some processing...
+		} catch (InterruptedException ex) {
+			System.err.println("Thread '" + Thread.currentThread().getName() + "' is interrupted!");
+		}
+	}
+}
+
+class Depositor implements Runnable {
+	private int amount;
+	private BankAccount bankAccount;
+
+	public Depositor(int amount, BankAccount bankAccount) {
+		this.amount = amount;
+		this.bankAccount = bankAccount;
+	}
+
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + " is executing and depositing Rs." + amount + "...");
+		bankAccount.deposit(amount);
+		System.out.println(Thread.currentThread().getName() + " is done executing and depositing.");
+	}
+
+}
